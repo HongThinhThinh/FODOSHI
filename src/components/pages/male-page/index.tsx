99bc275db2e09a3387c041c184d00a554e73b5a9
dovanminh <minhlola28@gmail.com>
2025-02-26 04:28:03 +0700

    create male-page and cofig new-product-page

---
+++ src/components/pages/male-page/index.tsx
@@ -0,0 +1,104 @@
+import { Button, Col, Collapse, Input, Row } from "antd";
+import "./styles.scss";
+import { showCardModel } from "../../../assets/model";
+import ShowCard from "../../atoms/show-card";
+import { SearchOutlined } from "@ant-design/icons";
+import InputComponent from "../../atoms/input";
+import { bannerMalePage } from "../../../assets/contant";
+const { Panel } = Collapse;
+
+function MalePage() {
+  const menuItems = {
+    category: ["Quần áo", "Giày dép", "Túi xách", "Phụ kiện"],
+    gender: ["Nam", "Nữ"],
+    brand: ["Thương hiệu A", "Thương hiệu B", "Thương hiệu C"],
+    price: ["Dưới 500k", "Từ 500k - 1 triệu", "Trên 1 triệu"],
+    stockStatus: ["Còn hàng", "Hết hàng"],
+    sale: ["Đang giảm giá"],
+  };
+
+  return (
+    <>
+      <section className="male-page-banner__container">
+        <img className="male-page-banner__img" src={bannerMalePage} alt="" />
+      </section>
+      <section className="male-page__container">
+        <Row className="male-page__wrapper">
+          <Col className="male-page__aside" span={5}>
+            <div className="male-page__aside__item rounded-full">
+              <span className="border-2 border-black py-1 px-4 rounded-full">
+                <SearchOutlined className="text-black  text-[20px]" />
+              </span>
+              <InputComponent
+                placeholder="Tìm kiếm sản phẩm"
+                shape="round"
+                bgColor="#d9d9d9"
+              />
+            </div>
+
+            <Collapse defaultActiveKey={[]} accordion={false}>
+              <Panel header="Phân loại" key="1">
+                {menuItems.gender.map((item, index) => (
+                  <Button key={index} type="text" className="filter-button">
+                    {item}
+                  </Button>
+                ))}
+              </Panel>
+
+              <Panel header="Danh mục" key="2">
+                {menuItems.category.map((item, index) => (
+                  <Button key={index} type="text" className="filter-button">
+                    {item}
+                  </Button>
+                ))}
+              </Panel>
+
+              <Panel header="Thương hiệu" key="3">
+                {menuItems.brand.map((item, index) => (
+                  <Button key={index} type="text" className="filter-button">
+                    {item}
+                  </Button>
+                ))}
+              </Panel>
+
+              <Panel header="Mức giá" key="4">
+                {menuItems.price.map((item, index) => (
+                  <Button key={index} type="text" className="filter-button">
+                    {item}
+                  </Button>
+                ))}
+              </Panel>
+
+              <Panel header="Tình trạng đỡ" key="5">
+                {menuItems.stockStatus.map((item, index) => (
+                  <Button key={index} type="text" className="filter-button">
+                    {item}
+                  </Button>
+                ))}
+              </Panel>
+
+              <Panel header="Sale" key="6">
+                {menuItems.sale.map((item, index) => (
+                  <Button key={index} type="text" className="filter-button">
+                    {item}
+                  </Button>
+                ))}
+              </Panel>
+            </Collapse>
+          </Col>
+          <Col className="male-page__products" span={19}>
+            <Row justify="space-between" wrap gutter={[20, 20]}>
+              {showCardModel?.map((item) => (
+                <Col>
+                  <ShowCard key={item.id} card={item} />
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </section>
+    </>
+  );
+}
+
+export default MalePage;
--- src/components/pages/new-product-page/index.tsx
+++ src/components/pages/new-product-page/index.tsx
@@ -1,76 +1,86 @@
-import { Button, Checkbox, Col, Collapse, Dropdown, Input, Menu, Row } from "antd";
+import { Button, Col, Collapse, Input, Row } from "antd";
 import "./styles.scss";
 import { showCardModel } from "../../../assets/model";
 import ShowCard from "../../atoms/show-card";
-import { useState } from "react";
-
-const { Search } = Input;
+import { SearchOutlined } from "@ant-design/icons";
+import InputComponent from "../../atoms/input";
 const { Panel } = Collapse;
 
 function NewProductPage() {
-
-
-
-
   const menuItems = {
-    category: ['Quần áo', 'Giày dép', 'Túi xách', 'Phụ kiện'],
-    gender: ['Nam', 'Nữ'],
-    brand: ['Thương hiệu A', 'Thương hiệu B', 'Thương hiệu C'],
-    price: ['Dưới 500k', 'Từ 500k - 1 triệu', 'Trên 1 triệu'],
-    stockStatus: ['Còn hàng', 'Hết hàng'],
-    sale: ['Đang giảm giá']
+    category: ["Quần áo", "Giày dép", "Túi xách", "Phụ kiện"],
+    gender: ["Nam", "Nữ"],
+    brand: ["Thương hiệu A", "Thương hiệu B", "Thương hiệu C"],
+    price: ["Dưới 500k", "Từ 500k - 1 triệu", "Trên 1 triệu"],
+    stockStatus: ["Còn hàng", "Hết hàng"],
+    sale: ["Đang giảm giá"],
   };
   return (
     <section className="new-product-page__container">
       <Row className="new-product-page__wrapper">
-      <Col className="new-product-page__aside" span={4}>
-      <div className="new-product-page__aside__item">
-        <span className="new-product-page__aside__item--title">Tìm kiếm sản phẩm</span>
-        <Search
-          placeholder="Tìm kiếm sản phẩm"
-          enterButton
-        />
-      </div>
+        <Col className="new-product-page__aside" span={5}>
+          <div className="new-product-page__aside__item rounded-full">
+            <span className="border-2 border-black py-1 px-4 rounded-full">
+              <SearchOutlined className="text-black  text-[20px]" />
+            </span>
+            <InputComponent
+              placeholder="Tìm kiếm sản phẩm"
+              shape="round"
+              bgColor="#d9d9d9"
+            />
+          </div>
 
-      <Collapse defaultActiveKey={[]} accordion={false}>
-        <Panel header="Phân loại" key="1">
-          {menuItems.gender.map((item, index) => (
-            <Button key={index} type="text" className="filter-button">{item}</Button>
-          ))}
-        </Panel>
+          <Collapse defaultActiveKey={[]} accordion={false}>
+            <Panel header="Phân loại" key="1">
+              {menuItems.gender.map((item, index) => (
+                <Button key={index} type="text" className="filter-button">
+                  {item}
+                </Button>
+              ))}
+            </Panel>
 
-        <Panel header="Danh mục" key="2">
-          {menuItems.category.map((item, index) => (
-            <Button key={index} type="text" className="filter-button">{item}</Button>
-          ))}
-        </Panel>
+            <Panel header="Danh mục" key="2">
+              {menuItems.category.map((item, index) => (
+                <Button key={index} type="text" className="filter-button">
+                  {item}
+                </Button>
+              ))}
+            </Panel>
 
-        <Panel header="Thương hiệu" key="3">
-          {menuItems.brand.map((item, index) => (
-            <Button key={index} type="text" className="filter-button">{item}</Button>
-          ))}
-        </Panel>
+            <Panel header="Thương hiệu" key="3">
+              {menuItems.brand.map((item, index) => (
+                <Button key={index} type="text" className="filter-button">
+                  {item}
+                </Button>
+              ))}
+            </Panel>
 
-        <Panel header="Mức giá" key="4">
-          {menuItems.price.map((item, index) => (
-            <Button key={index} type="text" className="filter-button">{item}</Button>
-          ))}
-        </Panel>
+            <Panel header="Mức giá" key="4">
+              {menuItems.price.map((item, index) => (
+                <Button key={index} type="text" className="filter-button">
+                  {item}
+                </Button>
+              ))}
+            </Panel>
 
-        <Panel header="Tình trạng đỡ" key="5">
-          {menuItems.stockStatus.map((item, index) => (
-            <Button key={index} type="text" className="filter-button">{item}</Button>
-          ))}
-        </Panel>
+            <Panel header="Tình trạng đỡ" key="5">
+              {menuItems.stockStatus.map((item, index) => (
+                <Button key={index} type="text" className="filter-button">
+                  {item}
+                </Button>
+              ))}
+            </Panel>
 
-        <Panel header="Sale" key="6">
-          {menuItems.sale.map((item, index) => (
-            <Button key={index} type="text" className="filter-button">{item}</Button>
-          ))}
-        </Panel>
-      </Collapse>
-    </Col>
-        <Col className="new-product-page__products" span={15}>
+            <Panel header="Sale" key="6">
+              {menuItems.sale.map((item, index) => (
+                <Button key={index} type="text" className="filter-button">
+                  {item}
+                </Button>
+              ))}
+            </Panel>
+          </Collapse>
+        </Col>
+        <Col className="new-product-page__products" span={19}>
           <Row justify="space-between" wrap gutter={[20, 20]}>
             {showCardModel?.map((item) => (
               <Col>
@@ -78,9 +88,6 @@ function NewProductPage() {
               </Col>
             ))}
           </Row>
-        </Col>
-        <Col className="new-product-page__cart" span={5}>
-        
         </Col>
       </Row>
     </section>
